02a6c6ba7107f0208df4e7d37d28bbe6901d7f15
Haxonopbot <haxonopbot@gmail.com>
2024-11-17 19:07:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "22717850"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "e84fb0ae1bbaf6f62b25e48a845cd804")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://haxonopbot:TIewU2ovEoKlVroq@cluster0.iiaij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
